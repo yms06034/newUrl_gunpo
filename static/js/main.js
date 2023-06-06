280be0cac17d7554727824b2522416bd083aef0a
Kim Sung Jin <yms06034@gmail.com>
2023-06-06 18:53:17 +0900

    Doing Page Design Scrits

--- static/js/main.js
+++ static/js/main.js
@@ -1,9 +1,6 @@
-const img_1 = document.getElementById("img_1");
-const img_2 = document.getElementById("img_2");
-const img_3 = document.getElementById("img_3");
-const img_4 = document.getElementById("img_4");
+const thisYear = document.querySelector('.this-year');
+thisYear.textContent = new Date().getFullYear();
 
-console.log(img_1);
-console.log(img_2);
-console.log(img_3);
-console.log(img_4);
+console.log(thisYear);
+
+
