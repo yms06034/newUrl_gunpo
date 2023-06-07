c0296919d5a26a5c6c341b9f14a6e46c61eacea2
Kim Sung Jin <yms06034@gmail.com>
2023-06-07 22:55:55 +0900

    Doing Main Page

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,13 @@
 const thisYear = document.querySelector('.this-year');
 thisYear.textContent = new Date().getFullYear();
 
+const location1 = document.querySelector('#bbbtn01');
+const traffic = document.querySelector('#bbbtn02');
+const life = document.querySelector('#bbbtn03');
+const education = document.querySelector('#bbbtn04');
+
+const img_location = document.querySelector('#img01');
+const img_traffic = document.querySelector('#img02');
+const img_life = document.querySelector('#img03');
+const img_education = document.querySelector('#img04');
 
